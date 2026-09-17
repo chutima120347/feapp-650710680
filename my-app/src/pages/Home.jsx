c6c3c516b0dcf2c6142a05a68fd62532a57ac522
chutima120347 <115211603+chutima120347@users.noreply.github.com>
2026-09-17 02:59:07 +0000

    update

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import { movies } from "../data";
 import MovieCard from "../components/MovieCard";
 import MovieGallery from "../components/MovieGallery";
